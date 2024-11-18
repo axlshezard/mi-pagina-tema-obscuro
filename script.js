98e9e722385ee4ef3b378ad6c076130a6f616346
Jc <jcgarciasolis@hotmail.com>
2024-11-18 15:14:56 -0600

    s

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const boton_tema = document.getElementById('boton_tema');
 boton_tema.addEventListener("click", () => {
     document.body.classList.toggle("tema-claro"); // Usamos el nombre de clase consistente
     if (document.body.classList.contains("tema-claro")) {
-        boton_tema.textContent = "Tema Oscuro"; // Texto correcto para alternar
+        boton_tema.textContent = "Obscuro"; // Texto correcto para alternar
     } else {
-        boton_tema.textContent = "Tema Claro";
+        boton_tema.textContent = "Claro";
     }
 });
